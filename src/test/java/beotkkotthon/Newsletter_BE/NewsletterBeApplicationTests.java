50da85902bd315535d4bcdf636fa32b7382d287f
zzimnii <102315673+zzimnii@users.noreply.github.com>
2024-03-14 20:50:37 +0900

    [CHORE] 프로젝트 기본 세팅 #1

---
+++ src/test/java/beotkkotthon/Newsletter_BE/NewsletterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package beotkkotthon.Newsletter_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
