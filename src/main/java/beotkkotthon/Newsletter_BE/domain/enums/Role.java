14866194d5daa3ae5b707c8856625a663e48cc71
sahyunjin <tkguswls1106@naver.com>
2024-03-18 05:51:42 +0900

    [FEAT] Participation 도메인 설계 #13

---
+++ src/main/java/beotkkotthon/Newsletter_BE/domain/enums/Role.java
@@ -0,0 +1,5 @@
+package beotkkotthon.Newsletter_BE.domain.enums;
+
+public enum Role {
+    LEADER, MEMBER, CREATOR
+}
--- src/main/java/beotkkotthon/Newsletter_BE/domain/mapping/Participation.java
+++ src/main/java/beotkkotthon/Newsletter_BE/domain/mapping/Participation.java
@@ -2,7 +2,6 @@ package beotkkotthon.Newsletter_BE.domain.mapping;
 
 import beotkkotthon.Newsletter_BE.domain.common.BaseEntity;
 import beotkkotthon.Newsletter_BE.domain.enums.RequestRole;
-import beotkkotthon.Newsletter_BE.domain.enums.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,14 +11,15 @@ import java.io.Serializable;
 @Getter
 @NoArgsConstructor
 
-@Table(name = "member_team")
+@Table(name = "participation")
 @Entity
-public class MemberTeam extends BaseEntity implements Serializable {
+public class Participation extends BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "request_role")
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private RequestRole requestRole;
 }
