457d254dcf554439556b7e9f928b189acd8a738d
zzimnii <102315673+zzimnii@users.noreply.github.com>
2024-03-21 02:50:44 +0900

    [FEATURE] description, MediaType 추가 #22

--- src/main/java/beotkkotthon/Newsletter_BE/web/controller/NewsController.java
+++ src/main/java/beotkkotthon/Newsletter_BE/web/controller/NewsController.java
@@ -9,7 +9,10 @@ import beotkkotthon.Newsletter_BE.service.NewsService;
 import beotkkotthon.Newsletter_BE.web.dto.request.NewsSaveRequestDto;
 import beotkkotthon.Newsletter_BE.web.dto.response.NewsResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,8 +27,9 @@ public class NewsController {
     private final NewsService newsService;
     private final NewsCheckService newsCheckService;
 
-    @PostMapping("/teams/{teamId}/news")
+    @PostMapping(value = "/teams/{teamId}/news", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.MULTIPART_FORM_DATA_VALUE})
     @Operation(summary = "가정통신문 발행")
+    @Parameter(name = "teamId", description = "팀의 아이디, path variable 입니다.")
     public ApiResponse<NewsResponseDto> createNews(
             @PathVariable(name = "teamId") Long teamId,
             @RequestPart(value = "writerId", required = true) Long memberId,
@@ -39,7 +43,7 @@ public class NewsController {
 
     @GetMapping("/teams/news")
     @Operation(summary = "가정통신문 목록 모두 조회(하단그룹-메인)")
-    public ApiResponse<NewsResponseDto.ShowNewsListDto> findAllNews(@RequestPart(name = "memberId") Long memberId) {
+    public ApiResponse<NewsResponseDto.ShowNewsListDto> findAllNews(@Parameter(description = "memberId") @RequestPart(name = "memberId") Long memberId) {
         List<News> newsList = newsService.findAll();
         List<NewsCheck> newsCheckList = newsCheckService.findByMember(memberId);
         return ApiResponse.onSuccess(NewsConverter.toShowNewsDtoList(newsList, newsCheckList));
@@ -47,6 +51,7 @@ public class NewsController {
 
     @GetMapping("/teams/{teamId}/news")
     @Operation(summary = "팀별 가정통신문 조회")
+    @Parameter(name = "teamId", description = "팀의 아이디, path variable 입니다.")
     public ApiResponse<List<NewsResponseDto>> findNewsByTeam(@PathVariable(name = "teamId") Long teamId) {
         List<NewsResponseDto> newsResponseDtos = newsService.findNewsByTeam(teamId);
         return ApiResponse.onSuccess(newsResponseDtos);
@@ -54,7 +59,11 @@ public class NewsController {
 
     @GetMapping("/teams/{teamId}/news/{newsId}")
     @Operation(summary = "가정통신문 상세 조회")
-    public ApiResponse<NewsResponseDto.ShowNewsDto> findNewsById(@RequestPart(name = "memberId") Long memberId,
+    @Parameters({
+            @Parameter(name = "teamId", description = "팀의 아이디, path variable 입니다."),
+            @Parameter(name = "newsId", description = "가정통신문의 아이디, path variable 입니다.")
+    })
+    public ApiResponse<NewsResponseDto.ShowNewsDto> findNewsById(@Parameter(description = "memberId") @RequestPart(name = "memberId") Long memberId,
                                                                  @PathVariable(name = "teamId") Long teamId,
                                                                  @PathVariable(name = "newsId") Long newsId) {
         newsCheckService.readNews(memberId, newsId);
@@ -63,7 +72,7 @@ public class NewsController {
 
     @GetMapping("/news")
     @Operation(summary = "미확인 가정통신문 전체 목록 조회")
-    public ApiResponse<List<NewsResponseDto>> notReadNews(@RequestPart(name = "memberId") Long memberId,
+    public ApiResponse<List<NewsResponseDto>> notReadNews(@Parameter(description = "memberId") @RequestPart(name = "memberId") Long memberId,
                                                           @RequestParam(name = "teamId", required = false) Long teamId) {
         List<NewsResponseDto> notReadNewsList = newsService.notReadNewslist(memberId, teamId);
         return ApiResponse.onSuccess(notReadNewsList);
@@ -71,6 +80,7 @@ public class NewsController {
 
     @GetMapping("/{memberId}/news")
     @Operation(summary = "내가 발행한 가정통신문 목록 조회")
+    @Parameter(name = "memberId", description = "멤버의 아이디, path variable 입니다.")
     public ApiResponse<List<NewsResponseDto>> findNewsByWriter(@PathVariable(name = "memberId") Long memberId,
                                                                @RequestParam(name = "teamId", required = false) Long teamId) {
         List<NewsResponseDto> newsResponseDtos = newsService.findNewsByMember(memberId, teamId);
