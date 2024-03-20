a55632043fafad875f3b498681a039b73141bdc3
sahyunjin <tkguswls1106@naver.com>
2024-03-20 19:31:38 +0900

    [FEAT] 팀 가입 요청 목록 조회 컨트롤러 작성 #23

--- src/main/java/beotkkotthon/Newsletter_BE/web/controller/ParticipationController.java
+++ src/main/java/beotkkotthon/Newsletter_BE/web/controller/ParticipationController.java
@@ -1,9 +1,13 @@
 package beotkkotthon.Newsletter_BE.web.controller;
 
+import beotkkotthon.Newsletter_BE.payload.ApiResponse;
 import beotkkotthon.Newsletter_BE.service.ParticipationService;
+import beotkkotthon.Newsletter_BE.web.dto.response.NewsResponseDto;
+import beotkkotthon.Newsletter_BE.web.dto.response.ParticipationResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -13,5 +17,9 @@ public class ParticipationController {
     private final ParticipationService participationService;
 
 
-
+    @PostMapping("/teams/{teamId}/participations")
+    public ApiResponse<List<ParticipationResponseDto>> findParticipationByTeam(@PathVariable Long teamId) {
+        List<ParticipationResponseDto> participationResponseDtoList = participationService.findParticipationByTeam(teamId);
+        return ApiResponse.onSuccess(participationResponseDtoList);
+    }
 }
