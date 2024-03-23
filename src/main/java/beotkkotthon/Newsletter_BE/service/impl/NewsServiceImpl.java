158ef66bfa5a24b2de6e855e56924ddef4bbf1d5
sahyunjin <tkguswls1106@naver.com>
2024-03-23 18:09:30 +0900

    [FIX] ELB Deploy 문제 #113

--- src/main/java/beotkkotthon/Newsletter_BE/service/impl/NewsServiceImpl.java
+++ src/main/java/beotkkotthon/Newsletter_BE/service/impl/NewsServiceImpl.java
@@ -26,7 +26,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
